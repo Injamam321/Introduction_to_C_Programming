6796bfb41b53080ea19404a46a93399e3f6ad0b3
Md. Injamam Hossain <injamam075@gmail.com>
2024-12-05 20:54:05 +0600

    5-12-24

---
+++ Week_03(array operation)/module_10/initialize.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+
+    char a[8]="imjamam";//NULL GARUNTEE
+
+     char b[8]={'i','m','j','a','m','a','m'};//NULL NO GARUNTEE
+    // for (int i = 0; i < 7; i++)
+    // {
+    //     /* code */printf("%C ", a[i]);
+    // }
+    printf("%s \n", a);
+    printf("%s", b);
+   
+    return 0;
+}
