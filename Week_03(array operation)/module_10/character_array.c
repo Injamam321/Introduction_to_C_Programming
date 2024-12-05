aa3430de102ae637679bcb42b9546c33cd716d87
Md. Injamam Hossain <injamam075@gmail.com>
2024-12-05 19:56:59 +0600

    5-12-24

---
+++ Week_03(array operation)/module_10/character_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char a[5];
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%c",&a[i]);
+
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%c ",a[i]);
+    }
+    
+    
+    return 0;
+
+}
