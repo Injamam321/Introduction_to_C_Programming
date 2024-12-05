928d01a2675a37987d57d164a67334809805280e
Md. Injamam Hossain <injamam075@gmail.com>
2024-12-05 18:01:15 +0600

    5-12-24

--- Week_03(array operation)/module_09/array_reverse.c
+++ Week_03(array operation)/module_09/array_reverse.c
@@ -1,29 +1,68 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int ar[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &ar[i]);
+//     }
+//     int i=0, j=n-1;
+//     while (i<j)
+//     {
+//         int tmp=ar[i];
+//         ar[i]=ar[j];
+//         ar[j]=tmp;
+//         i++;
+//         j--;
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("%d ", ar[i]);
+//     }
+
+    
+//     return 0;
+// }
+
 #include <stdio.h>
-int main()
-{
+#include <stdlib.h>
+
+int main() {
     int n;
     scanf("%d", &n);
-    int ar[n];
-    for (int i = 0; i < n; i++)
-    {
+
+    // Dynamic array allocation
+    int *ar = (int *)malloc(n * sizeof(int));
+    if (ar == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    // Array input
+    for (int i = 0; i < n; i++) {
         scanf("%d", &ar[i]);
     }
-    int i=0, j=n-1;
-    while (i<j)
-    {
-        int tmp=ar[i];
-        ar[i]=ar[j];
-        ar[j]=tmp;
+
+    // Reverse the array
+    int i = 0, j = n - 1;
+    while (i < j) {
+        int tmp = ar[i];
+        ar[i] = ar[j];
+        ar[j] = tmp;
         i++;
         j--;
     }
-    for (int i = 0; i < n; i++)
-    {
+
+    // Output the reversed array
+    for (int i = 0; i < n; i++) {
         printf("%d ", ar[i]);
     }
-    
-    
+    printf("\n");
 
-    
+    // Free allocated memory
+    free(ar);
+
     return 0;
 }
